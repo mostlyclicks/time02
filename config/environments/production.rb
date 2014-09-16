c5946245b00686a1b09f9cda7a0d2569ef5a8150
Carlos Interone <carlos@mostlyclicks.com>
2014-09-16 14:02:17 -0500

    clone initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Ec02::Application.configure do
+Time02::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
